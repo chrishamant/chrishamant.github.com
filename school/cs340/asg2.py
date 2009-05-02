4a26b440f7cbc6a3526a12071930f9b320e550a4
Chris Hamant <chris@hamant.net>
2009-05-02 15:51:22 -0400

    finished simple Rational Class

--- school/cs340/asg2.py
+++ school/cs340/asg2.py
@@ -14,24 +14,18 @@ class Rational:
     """Class to represent rational numbers"""
     
     def __init__(self,num=1,dem=1):
-        numneg = demneg = False
-        if num < 0:
-            numneg = True
-        if dem < 0:
-            demneg = True
-        if num == 0 or num == 1:
-            self.num = num #no more processing
-        if dem == 0 or dem == 1:
-            self.dem = dem
-        
-        factorsOfNum = filter(lambda x:num%x==0,range(1,(num/2)+1))
-        factorsOfDem = filter(lambda x:dem%x==0,range(1,(dem/2)+1))
-        print factorsOfNum
-        print factorsOfDem
-        self.num = num
-        self.dem = dem
-        
-        
+        thegcd = self.gcd(num,dem)
+        self.num = num/thegcd
+        self.dem = dem/thegcd
+    
+    def gcd(self,num1, num2):
+        """gcd helper function"""
+        gcd = 1
+        for i in range(2,abs(num1)+1):
+            if num1 % i == 0 and num2 % i == 0:
+                gcd = i
+        return gcd
+    
     def __unicode__(self):
         return str(self.num) + "/" + str(self.dem)
     
@@ -39,18 +33,52 @@ class Rational:
         return str(self.num) + "/" + str(self.dem)
     
     def __add__(self, other):
-        pass
-    
+        other = self.getOther(other)
+        sum = self.num*other.dem + other.num*self.dem
+        newdem = other.dem*self.dem
+        return Rational(sum,newdem)
+        
     def __sub__(self, other):
-        pass
+        other = self.getOther(other)
+        sub = sum = self.num*other.dem - other.num*self.dem
+        newdem = other.dem*self.dem
+        return Rational(sub,newdem)
     
     def __mul__(self, other):
-        pass
+        return Rational(self.num*other.num,self.dem*other.dem)
     
+    def __div__(self, other):
+        other = Rational(other.dem,other.num)
+        return self*other
     
+    def toFloat(self):
+        return (self.num+0.0)/self.dem
+    
+    def getOther(self,other):
+        """didn't quite think through how to treat it if not rational. 
+        Problem didn't specify, so I just accept ints or Rational"""
+        toadd = other
+        if(other.__class__ == int):
+            toadd = Rational(other,1)
+        if(toadd.__class__ != Rational):
+            raise
+        return toadd
+    
+
 
 #expected to run as shell script
 if __name__ == "__main__":
-    test = Rational()
-    print test  
-
+    test = Rational(3,2)
+    test2 = Rational(1,6)
+    new =  test + test2
+    print new
+    print new.toFloat()
+    new2 = test - test2
+    print new2
+    print new2.toFloat()
+    new3 = test * test2
+    print new3
+    print new3.toFloat()
+    new4 = test / test2
+    print new4
+    print new4.toFloat()
